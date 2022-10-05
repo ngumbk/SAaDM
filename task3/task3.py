32c477c8d60b165d585d0606a35a643da45962fd
ngumbk <chulkov.nikita01@gmail.com>
2022-10-05 18:21:31 +0300

    task3: add input processing in the task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,10 +1,22 @@
-def main():
-    task()
+import csv
 
 
 # returns the list of nodes and their relation
-def task():
-    pass
+def task(csv_string):
+    result = []
+    csv_string = csv_string.split('\n')
+    if csv_string[-1] == '':
+        del csv_string[-1]
+    
+    print(csv_string)
+    return result
+
+
+def main():
+    with open('data.csv') as file:
+        csv_string = file.read()
+        result = task(csv_string)
+        print(result)
 
 
 if __name__ == '__main__':
