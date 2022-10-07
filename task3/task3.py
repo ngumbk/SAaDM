6365b49bd50e114cd0f2c92ef1b4aff184d153cd
ngumbk <chulkov.nikita01@gmail.com>
2022-10-07 19:07:18 +0300

    task3: fix graph initialization

--- task3/task3.py
+++ task3/task3.py
@@ -7,10 +7,10 @@ def task(csv_string):
     result = []
 
     # making adjacency list out of edge list
-    graph_start_nodes = re.findall(r'\d+,', csv_string)
-    for i in range(len(graph_start_nodes)):
-        graph_start_nodes[i] = int(graph_start_nodes[i][:-1])
-    graph = {i: [] for i in range(1, max(graph_start_nodes) + 1)}
+    graph_nodes = re.findall(r'\d+', csv_string)
+    for i in range(len(graph_nodes)):
+        graph_nodes[i] = int(graph_nodes[i])
+    graph = {i: [] for i in range(1, max(graph_nodes) + 1)}
 
     csv_string = csv_string.split('\n')
     if csv_string[-1] == '':
@@ -23,8 +23,20 @@ def task(csv_string):
     # main task
     r1, r2, r3, r4, r5 = [], [], [], [], []
 
-    
+    for key in graph:
+        if len(graph[key]) > 0:
+            r1.append(key)
+        for exit_node in graph[key]:
+            if exit_node not in r2:
+                r2.append(exit_node)
 
+    print(r1, r2, sep='\n')
+    for i in r1:
+        for exit_node_1 in graph[i]:
+            print(exit_node_1, end=' ')
+
+        print('\n')
+    
     return result
 
 
